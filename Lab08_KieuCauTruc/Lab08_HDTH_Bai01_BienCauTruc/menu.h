6b21cef93cbb29319d7d34daf83e5e180697dbaa
Nguyen Dinh Quoc Huy <quochuynguyendinh12345@gmail.com>
2024-10-30 20:51:20 +0700

    Add files via upload

---
+++ Lab08_KieuCauTruc/Lab08_HDTH_Bai01_BienCauTruc/menu.h
@@ -0,0 +1,69 @@
+// Khai bao nguyen mau cac ham xu ly menu
+void XuatMenu();
+int ChonMenu(int soMenu);
+void XuLyMenu(int menu, MangNV a, int &n);
+void ChayChuongTrinh(MangNV a, int& n);
+
+// Dinh nghia cac ham xu ly menu
+void XuatMenu()
+{
+	cout << endl << "======== CHON CHUC NANG ========";
+	cout << endl << "1. Nhap danh sach nhan vien moi";
+	cout << endl << "2. Xuat danh sach sinh vien";
+	cout << endl << "3. Them mot nhan vien moi";
+	cout << endl << "0. Thoat khoi chuong trinh";
+	cout << endl << "================================";
+}
+
+int ChonMenu(int soMenu)
+{
+	int stt;
+	do
+	{
+		system("cls");
+		XuatMenu();
+		cout << endl << "Nhap mot so de chon menu: "; cin >> stt;
+	} while (stt < 0 || stt > soMenu);
+	return stt;
+}
+
+void XuLyMenu(int menu, MangNV a, int& n)
+{
+	switch (menu)
+	{
+	case 1:
+		cout << endl << "1. Nhap danh sach nhan vien moi" << endl;
+		cout << endl << "Nhap so nhan vien: ";
+		cin >> n;
+		NhapDSNV(a, n);
+		break;
+	case 2:
+		cout << endl << "2. Xuat danh sach sinh vien" << endl;
+		XuatDanhSachNV(a, n);
+		break;
+	case 3:
+		cout << endl << "3. Them mot nhan vien moi" << endl;
+		ThemMotNV(a, n);
+		XuatDanhSachNV(a, n);
+		break;
+	default:
+		cout << endl << "Thoat khoi chuong trinh !";
+		break;
+	}
+	if (menu > 0)
+	{
+		cout << endl << "Nhap 1 phim bat ky de tiep tuc !";
+		_getch();
+	}
+}
+
+void ChayChuongTrinh(MangNV a, int& n)
+{
+	int menu, soMenu = 3;
+	do
+	{
+		menu = ChonMenu(soMenu);
+		XuLyMenu(menu, a, n);
+	} while (menu > 0);
+}
+
---
+++ Lab08_KieuCauTruc/Lab08_HDTH_Bai01_BienCauTruc/thuvien.h
@@ -0,0 +1,131 @@
+#define MAX 100
+#define TAB '\t'
+
+struct NgayThang
+{
+	unsigned short Ngay;
+	unsigned short Thang;
+	unsigned short Nam;
+};
+
+struct NhanVien
+{
+	unsigned int MaSo;
+	char HoTen[MAX];
+	char DiaChi[MAX];
+	NgayThang NgaySinh;
+	float HeSoLuong;
+};
+
+typedef	NhanVien MangNV[MAX];
+
+// Khai bao nguyen mau cac ham xu ly
+void XuatTieuDe();
+void XuatDongKeNgang();
+void XuatMotNV(NhanVien nv);
+void XuatDanhSachNV(MangNV a, int n);
+void NhapMotNV(NhanVien& nv);
+void NhapDSNV(MangNV a, int n);
+int KiemTraMaNV(MangNV a, int n, int maSo);
+void ThemMotNV(MangNV a, int& n);
+
+// Dinh nghia cac ham xu ly
+void XuatTieuDe()
+{
+	cout << endl
+		 << setiosflags(ios::left)
+		 << setw(6)  << "Ma so"
+		 << setw(25) << "Ho va ten"
+		 << setw(25) << "Dia chi"
+		 << setw(15) << "Ngay sinh"
+		 << setw(7)  << "HS Luong"
+		 << endl;
+}
+
+void XuatDongKeNgang()
+{
+	cout << setiosflags(ios::left)
+		 << setw(6)  << "====="
+		 << setw(25) << "========================"
+		 << setw(25) << "========================"
+		 << setw(15) << "=============="
+		 << setw(7)  << "======"
+		 << endl;
+}
+
+void XuatMotNV(NhanVien nv)
+{
+	cout << setiosflags(ios::left)
+		 << setw(6)  << nv.MaSo
+		 << setw(25) << nv.HoTen
+		 << setw(25) << nv.DiaChi
+		 << setw(2)  << nv.NgaySinh.Ngay << "/ "
+		 << setw(2)  << nv.NgaySinh.Thang << "/ "
+		 << setw(7)  << nv.NgaySinh.Nam
+		 << setw(7)  << nv.HeSoLuong
+		 << endl;
+}
+
+void XuatDanhSachNV(MangNV a, int n)
+{
+	XuatTieuDe();
+	XuatDongKeNgang();
+	
+	for (int i = 0; i < n; i++)
+		XuatMotNV(a[i]);
+	XuatDongKeNgang();
+	cout << endl;
+}
+
+void NhapMotNV(NhanVien& nv)
+{
+	cout << "Nhap ma so nhan vien: ";
+	cin >> nv.MaSo;
+
+	cout << "Nhap ho va ten: ";
+	cin.ignore(1);
+	gets_s(nv.HoTen);
+
+	cout << "Nhap dia chi: ";
+	cin.ignore(1);
+	gets_s(nv.DiaChi);
+
+	cout << "Nhap ngay sinh (thu tu ngay thang nam): ";
+	cin >> nv.NgaySinh.Ngay
+		>> nv.NgaySinh.Thang
+		>> nv.NgaySinh.Nam;
+
+	cout << "Nhap he so luong: ";
+	cin >> nv.HeSoLuong;
+}
+
+void NhapDSNV(MangNV a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << endl << "Nhan vien thu " << i + 1 << ": " << endl;
+		NhapMotNV(a[i]);
+	}
+	cout << endl << "Da nhap xong !" << endl;
+}
+
+int KiemTraMaNV(MangNV a, int n, int maSo)
+{
+	for (int i = 0; i < n; i++)
+		if (a[i].MaSo == maSo)
+			return 1;
+	return 0;
+}
+
+void ThemMotNV(MangNV a, int& n)
+{
+	NhanVien nv;
+	NhapMotNV(nv);
+	while (KiemTraMaNV(a, n, nv.MaSo))
+	{
+		cout << endl << "Ma so " << nv.MaSo << " da bi trung, nhap lai ma khac: ";
+		cin >> nv.MaSo;
+	}
+	a[n] = nv;
+	n++;
+}
